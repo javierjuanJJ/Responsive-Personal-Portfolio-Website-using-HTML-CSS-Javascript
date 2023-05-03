ecb8d06d0faa19b1672a55054a5de8e15dc0984a
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-05-03 20:02:48 +0200

    Commit 26

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 // toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navBar = document.querySelector('.navbar');
 
+menuIcon.onclick = function () {
+    menuIcon.classList.toggle('bx-x');
+    navBar.classList.toggle('active');
+}
 
 
 //scroll sections
@@ -14,13 +20,19 @@ window.onscroll = function () {
         let offset = value.offsetTop - 100;
         let height = value.offsetHeight;
         let id = value.getAttribute('id');
+        console.log(id);
 
-        if (top >= offset&& top < offset + height){
+        if (top >= offset && top < offset + height){
             navLinks.forEach(value1 => {
                 value1.classList.remove('active');
-                document.querySelector('header nav a[href= ' + id + ']').classList.add('active');
+                document.querySelector('header nav a[href=' + id + ']').classList.add('active');
 
-            })
+            });
+            // Active sections for animation on scroll
+            value.classList.add('show-animate');
+        }
+        else {
+            value.classList.remove('show-animate');
         }
 
     });
@@ -28,4 +40,11 @@ window.onscroll = function () {
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.screenY > 100);
+
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+    menuIcon.classList.remove('bx-x');
+    navBar.classList.remove('active');
+    let footer = document.querySelector('footer');
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
